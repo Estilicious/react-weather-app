341954800d755ee67b74a4b4c8859d8924ed42ec
Estilicious <81353867+Estilicious@users.noreply.github.com>
2021-09-02 21:38:01 +0900

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        © <a href="https://esty.netlify.app" target="_blank">Esther Chukwu </a> 2021 and is{" "} 
+      <a 
+      href="https://github.com/Estilicious/react-weather-app" target="_blank">
+     open-sourced on github</a>
+    </footer>
     </div>
   );
 }
