5ef73c8e41dc0afb388203fefa2b0627f6418eb2
Estilicious <81353867+Estilicious@users.noreply.github.com>
2021-09-03 21:55:58 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <footer>
         © <a href="https://esty.netlify.app" target="_blank" rel="noreferrer" >Esther Chukwu </a> 2021 and is{" "} 
@@ -10,6 +11,7 @@ export default function App() {
       href="https://github.com/Estilicious/react-weather-app" target="_blank" rel="noreferrer">
      open-sourced on github</a>
     </footer>
+    </div>
     </div>
   );
 }
