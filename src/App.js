0cdfbd97f0919b65ba4c47df9ce4706aebb33ab2
Estilicious <81353867+Estilicious@users.noreply.github.com>
2021-09-13 21:56:21 +0900

    Added an external link to App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         © <a href="https://esty.netlify.app" target="_blank" rel="noopener noreferrer" >Esther Chukwu </a> 2021 and is{" "} 
       <a 
       href="https://github.com/Estilicious/react-weather-app" target="_blank" rel="noopener noreferrer">
-     open-sourced on github</a>
+     open-sourced on Github</a> and <a href= "https://app.netlify.com/sites/hopeful-lichterman-89386c/deploys" target="_blank" rel="noopener noreferrer">Netlify</a> 
     </footer>
     </div>
     </div>
